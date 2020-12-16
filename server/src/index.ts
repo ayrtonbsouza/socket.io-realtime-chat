975549b6930f2d258cc74e5501d68c831369e797
Ayrton Souza <ayrtonsouza@icloud.com>
2020-12-16 17:49:35 -0300

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import cors from 'cors';
 
 import router from './routes';
 
@@ -13,12 +14,15 @@ const io = new Server(server);
 io.on('connection', socket => {
   console.log('We have a new connection');
 
+  socket.on('join', ({ name, room }, callback) => { });
+
   socket.on('disconnect', () => {
     console.log('User had left');
   });
 });
 
 app.use(router);
+app.use(cors());
 
 server.listen(PORT, () => {
   console.log(`Server has started and it's running on port ${PORT} 🚀`);
