6ed00405418f3fb34396fcabaec7ada75a47bfc1
Ayrton Souza <ayrtonsouza@icloud.com>
2020-12-16 15:46:51 -0300

    join page development

--- client/src/components/Join/index.tsx
+++ client/src/components/Join/index.tsx
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Container, InnerContainer, JoinInput } from './styles';
+import { Container, InnerContainer, JoinInput, Header } from './styles';
 
 const Join: React.FC = () => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
 
-  function handleChange(): void {
-    console.log('change');
-  }
-
   return (
     <Container>
       <InnerContainer>
-        <h1>Join</h1>
+        <Header>Join</Header>
         <div>
-          <JoinInput type="text" placeholder="Name" onChange={handleChange} />
+          <JoinInput
+            type="text"
+            placeholder="Name"
+            onChange={event => setName(event.target.value)}
+          />
         </div>
         <div>
-          <JoinInput type="text" placeholder="Room" onChange={handleChange} />
+          <JoinInput
+            type="text"
+            placeholder="Room"
+            onChange={event => setRoom(event.target.value)}
+          />
         </div>
-        <Link to="/chat">
+        <Link
+          onClick={event => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
           <button type="submit">Sign In</button>
         </Link>
       </InnerContainer>
--- client/src/components/Join/styles.ts
+++ client/src/components/Join/styles.ts
@@ -1,5 +1,53 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-export const InnerContainer = styled.div``;
-export const JoinInput = styled.input``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+  align-items: center;
+  background-color: #1a1a1d;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    height: 100%;
+  }
+
+  button {
+    margin-top: 20px;
+    color: #fff !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    background: #ff9000;
+    padding: 20px;
+    border-radius: 5px;
+    display: inline-block;
+    border: none;
+    width: 100%;
+  }
+
+  button:focus {
+    outline: 0;
+  }
+`;
+
+export const InnerContainer = styled.div`
+  width: 50%;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    height: 90%;
+  }
+`;
+
+export const JoinInput = styled.input`
+  margin-top: 20px;
+  border-radius: 5px;
+  padding: 15px 20px;
+  width: 100%;
+`;
+
+export const Header = styled.h1`
+  color: white;
+  font-size: 2.5em;
+  padding-bottom: 10px;
+  border-bottom: 2px solid white;
+`;
