1c3074238d7a14531095df52aefa92ffb4c54dee
Ayrton Souza <ayrtonsouza@icloud.com>
2020-12-16 16:47:04 -0300

    socket.io connection development

--- client/src/components/Chat/index.tsx
+++ client/src/components/Chat/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import { io } from 'socket.io-client';
 
 import { Container } from './styles';
 
-const Chat: React.FC = () => {
+let socket;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Chat: React.FC = ({ location }: any) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    // eslint-disable-next-line no-shadow
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name as string);
+    setRoom(room as string);
+
+    socket.emit('join', { name, room }, () => {
+      // eslint-disable-next-line no-console
+      console.log(name, room);
+    });
+
+    return () => {
+      socket.off();
+    };
+  }, [ENDPOINT, location.search, name, room]);
+
   return (
     <Container>
       <h1>Chat</h1>
